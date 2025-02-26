0c3e37ffc94b9bd49cf2c4cc5faa707620193db3
My-kull <hurst-maxims.0m@icloud.com>
2025-02-26 12:31:51 +0200

    fix

--- CAFG.py
+++ CAFG.py
@@ -1,23 +1,9 @@
 import mysql.connector
 
-# Connect to MySQL
+# Connect to MariaDB
 conn = mysql.connector.connect(
-    host="localhost",
-    user="surviver",
-    password="123",
-    database="flight_game",
+    host="localhost", user="surviver", password="123", database="flight_game"
 )
 cursor = conn.cursor()
 
-# Read and execute the SQL file
-with open("flight_game.sql", "r") as file:
-    sql_script = file.read()
-    for statement in sql_script.split(";"):  # Split by statements
-        if statement.strip():  # Ignore empty statements
-            cursor.execute(statement)
-
-conn.commit()
-conn.close()
-
-print("SQL file executed successfully.")
-
+cursor.execute("SELECT * from airport;")
