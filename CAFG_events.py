76feececc3a6fd332b4d08819125bb292fb199df
vilmarvalval <ville.valtasalo@outlook.com>
2025-03-03 12:33:33 +0200

    Added class "Event"

---
+++ CAFG_events.py
@@ -0,0 +1,14 @@
+class Event:
+    desc = ""
+    rarity= ""
+
+    def __init__(self,name):
+        self.name = name
+
+nationalhero = Event("National Hero")
+nationalhero.desc = ""
+nationalhero.rarity = ""
+
+space_express = Event("Space Express")
+space_express.desc = ""
+space_express.rarity = ""
