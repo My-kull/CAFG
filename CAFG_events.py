42ced5e648c329d217cba84a49a2d5a15f56b5fe
vilmarvalval <ville.valtasalo@outlook.com>
2025-03-04 15:24:24 +0200

    Added attribute 'location' to Events

--- CAFG_events.py
+++ CAFG_events.py
@@ -1,14 +1,16 @@
 class Event:
     desc = ""
     rarity= ""
-
+    location= ""
     def __init__(self,name):
         self.name = name
 
-nationalhero = Event("National Hero")
-nationalhero.desc = ""
-nationalhero.rarity = ""
+national_hero = Event("National Hero")
+national_hero.desc = ""
+national_hero.rarity = "Harvinainen"
+national_hero.location = "everywhere"
 
 space_express = Event("Space Express")
-space_express.desc = ""
-space_express.rarity = ""
+space_express.desc = "Gives a random item."
+space_express.rarity = "Harvinainen"
+space_express.location = "everywhere"
