fcc4bbe20163f0e6f60a1d06186852a08065cdf3
vilmarvalval <ville.valtasalo@outlook.com>
2025-03-06 12:06:59 +0200

    Updated descryptions (item.desc) and added comments on attributes for clarity.

--- CAFG_items.py
+++ CAFG_items.py
@@ -6,8 +6,21 @@ cheap = 10
 very_cheap = 2
 free = 0
 
+
+# About attribute.rarity: Items rarity placeholder.
+#   Rarities ranging from most common to most rare:
+#       Todella yleinen.
+#       Yleinen.
+#       Semi harvinainen.
+#       Harvinainen.
+#       Erittäin harvinainen.
+
+# About
+
+# About attribute.use_time: positive [int]s give items the corresponding amount of use times
+#   (Once 0 item cant be used anymore). -1 gives an infinite amount of use times.
+
 # About attribute.active: True means item is active, False means item is passive.
-# About rarity
 
 
 class Item:
@@ -21,7 +34,10 @@ class Item:
 
 
 invis_cape = Item("Invisibility Cloack")
-invis_cape.desc = "A cloack that makes you invisible.\nWhen used, splits the local threat level in two\n(-50% local threat.)."
+invis_cape.desc = ("A cloack that makes you invisible.\n"
+                   "When used, splits the local threat level in two\n"
+                   "\n"
+                   "(-50% local threat.).")
 invis_cape.rarity = "Harvinainen"
 invis_cape.price = costly
 invis_cape.use_time = 1
@@ -29,7 +45,11 @@ invis_cape.active = True
 #[local threat]/2 when used
 
 lottery = Item("Falsified Lottery Coupon")
-lottery.desc = "With your skills- I mean luck, you modify-\n*ahem* you SOMEHOW manage to get a lottery coupon with the winning numbers!\nLucky you!"
+lottery.desc = ("With your skills- I mean luck, you modify-\n"
+                "*ahem* you SOMEHOW manage to get a lottery coupon with the winning numbers!\n"
+                "Lucky you!\n"
+                "\n"
+                "(Gives like a 1000€)")  #PLACEHOLDER!!!!!!!!!!!!!!!!!!!!11
 lottery.rarity = "Semi-harvinainen"
 lottery.price = expensive
 lottery.use_time = 1
@@ -37,7 +57,8 @@ lottery.type = True
 #Gives like 1000€
 
 luck_cookie = Item("Fortune Cookie")
-luck_cookie.desc = "A traditional chinese cookie that tells your fortune!\n(+luck)"
+luck_cookie.desc = ("A traditional chinese cookie that tells your fortune!\n"
+                    "\n(+luck)")
 luck_cookie.rarity = "Yleinen"
 luck_cookie.price = cheap
 luck_cookie.use_time = 1
@@ -45,7 +66,8 @@ luck_cookie.active = True
 #Increases luck, possibly add a small chance to decrease it instead?
 
 s_rabbit_paw = Item("Space-rabbit Foot")
-s_rabbit_paw.desc = "Straight from the vast prairies of space."
+s_rabbit_paw.desc = ("Straight from the vast prairies of space.\n"
+                     "\n(+luck)")
 s_rabbit_paw.rarity = "Harvinainen"
 s_rabbit_paw.price = expensive
 s_rabbit_paw.use_time = -1
@@ -53,7 +75,8 @@ s_rabbit_paw.active = True
 #Adds a certain % buff to luck.
 
 janitor = Item("Janitors Clothes")
-janitor.desc = "Allows you to disguise yourself as a janitor and work for some money."
+janitor.desc = ("Allows you to disguise yourself as a janitor and work for some money.\n"
+                "\n(Increases income from 'Clean Airport' [job])")
 janitor.rarity = "Yleinen"
 janitor.price = costly
 janitor.use_time = -1
@@ -62,7 +85,11 @@ janitor.active = False
 
 #below this, items are not used
 flightmaster = Item("Flight-masters Clothes")
-flightmaster.desc = "Allows you to disguise your self as a flight master\nand earn money while flying. Due to you not knowing how to fly however,\nthere is a slight chance you'll fall out of the sky(0.1%)."
+flightmaster.desc = ("Allows you to disguise your self as a flight master\n"
+                     "and earn money while flying. Due to you not knowing how to fly however,\n"
+                     "there is a slight chance you'll fall out of the sky.\n"
+                     "\n"
+                     "(0.1% chance to die on the next flight.)%")
 flightmaster.rarity = "Harvinainen"
 flightmaster.price = expensive
 flightmaster.use_time = -1
@@ -78,7 +105,12 @@ bulletvest.active = True
 #
 
 tonnin_seteli = Item("Kahvi + keksi")
-tonnin_seteli.desc = "You think you may have gotten scammed...\nin fact you are pretty sure you did get scammed.\nYou did give a 1000€, right? There is no way it wasn't 1000€...\nIt was 1000€ you gave, didn't you? It was 1000€... \n\x1b[3m'Se oli tonnin seteli… enks mä antanu tonnin setelin?'\x1b[0m"
+tonnin_seteli.desc = ("You think you may have gotten scammed...\n"
+                      "in fact you are pretty sure you did get scammed.\n"
+                      "You did give a 1000€, right? There is no way it wasn't 1000€...\n"
+                      "It was 1000€ you gave, didn't you? It was 1000€... \n"
+                      "\n"
+                      "\x1b[3m'Se oli tonnin seteli… enks mä antanu tonnin setelin?'\x1b[0m")
 tonnin_seteli.rarity = "Erittäin harvinainen"
 tonnin_seteli.price = 1000  # Tonnin seteli...
 tonnin_seteli.use_time = -1
@@ -86,17 +118,24 @@ tonnin_seteli.active = False
 #Does absolutely nothing, reference to 'Kummeli'
 
 warhead = Item("Unstable Nuclear Warhead")
-warhead.desc = "A Nuclear Warhead. Obviously no one wants to arrest a man with an armed bomb.\nDue to its unstable nature however, there is a 0.5% chance it will detonate.\n\x1b[3mOh and by the way, don't ask how you are allowed onto ANY planes with THIS...\x1b[0m\n(Local threat stays at 0, -5% global threat)"
+warhead.desc = ("A Nuclear Warhead. Obviously no one wants to arrest a man with an armed bomb.\n"
+                "Due to its unstable nature however, there is a 0.5% chance it will detonate.\n"
+                "\n"
+                "\x1b[3mOh and by the way, don't ask how you are allowed onto ANY planes with THIS...\x1b[0m\n"
+                "\n"
+                "(Local threat stays at 0, -5% global threat. 0.5% chance to end the game on each turn.)")
 warhead.rarity = "Erittäin Harvinainen"
 warhead.price = free  # it's free!
 warhead.use_time = -1
 warhead.active = False
 #0.5% for game to end. Local threat stays at 0, global threat grows by -5%
-
-#On a random turn= "The nuclear warhead is shaking!"
-#throw d20= 2-20 = "Nothing happened...", 1 = "Game over"
+#Alternatively: On start of a turn, theres ~1-5% to print= "The nuclear warhead is shaking!"
+#Then game throws a d20= if 2-20 = print "Nothing happened...", if 1 = "Game over"
 
 shop_items = [invis_cape, lottery, luck_cookie, s_rabbit_paw, janitor]
 
 #for i in shop_items:
-#    print(f"{i.name:<30} --- {i.price:>10}")
+#   print(f"{i.name:<30} --- {i.price:>10}")
+
+for i in shop_items:
+    print(f"{i.name} \n {i.desc} \n -------------------------------------------------------------")
