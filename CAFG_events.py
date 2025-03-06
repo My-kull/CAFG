0bca269df90635bf96eac4cc37e3093bec3ed51a
vilmarvalval <ville.valtasalo@outlook.com>
2025-03-06 12:13:53 +0200

    Added event "Northern Lights"

--- CAFG_events.py
+++ CAFG_events.py
@@ -5,12 +5,21 @@ class Event:
     def __init__(self,name):
         self.name = name
 
+#decreases local_threat by set amount or %
+fox_fires = Event("Northern Lights")
+fox_fires.desc = ""
+fox_fires.rarity = "Semi Harvinainen"
+fox_fires.location = "country dependent"
+
+
+#Sets local_threat to 0 if successful
 national_hero = Event("National Hero")
 national_hero.desc = ""
 national_hero.rarity = "Harvinainen"
 national_hero.location = "everywhere"
 
+#Gives a random item
 space_express = Event("Space Express")
-space_express.desc = "Gives a random item."
+space_express.desc = ""
 space_express.rarity = "Harvinainen"
 space_express.location = "everywhere"
