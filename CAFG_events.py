603741987ab9a52f4816345c1144a7122f310be0
vilmarvalval <ville.valtasalo@outlook.com>
2025-03-11 10:23:17 +0200

    Added text to event.desc

--- CAFG_events.py
+++ CAFG_events.py
@@ -7,20 +7,20 @@ class Event:
 
 #decreases local_threat by set amount or %
 fox_fires = Event("Northern Lights")
-fox_fires.desc = ""
+fox_fires.desc = "Northern lights can be seen in the sky."
 fox_fires.rarity = "Semi Harvinainen"
 fox_fires.location = "country dependent"
 
 
 #Sets local_threat to 0 if successful
 national_hero = Event("National Hero")
-national_hero.desc = ""
+national_hero.desc = "A terror attack is about to happen at the airport!"
 national_hero.rarity = "Harvinainen"
 national_hero.location = "everywhere"
 
 #Gives a random item
 space_express = Event("Space Express")
-space_express.desc = ""
+space_express.desc = "Your Qawason express package has arrived! You wonder when you ordered this..."
 space_express.rarity = "Harvinainen"
 space_express.location = "everywhere"
 
