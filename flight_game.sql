ebafbe3c8d0a6fae90d314f1bd4a7d36a80d0e51
My-kull <hurst-maxims.0m@icloud.com>
2025-03-12 15:22:53 +0200

    bugit korjattu

--- flight_game.sql
+++ flight_game.sql
@@ -189,8 +189,7 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-09-10 10:42:06
 
-DELETE FROM gloal_reached;
-DELETE FROM game;
-DELETE FROM airport WHERE type != "large_airport";
+DELETE FROM airport WHERE type <> 'large_airport';
+
+-- Dump completed on 2022-09-10 10:42:06
